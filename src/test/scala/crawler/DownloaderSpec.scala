b266f2c41b1513fb11ca76175cb1be3306c139dd
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-10-24 21:07:59 +0100

    with futures

--- src/test/scala/crawler/DownloaderSpec.scala
+++ src/test/scala/crawler/DownloaderSpec.scala
@@ -3,15 +3,23 @@ package crawler
 import org.scalatest.FunSuite
 import sttp.client.quick._
 import scalaj.http._
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.concurrent.Future
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class DownloaderSpec extends FunSuite {
   test("Can download url"){
 //    val result = basicRequest.get(uri"https://google.com").send()
 //    println(result.body)
 
+    val futureBody = Future{
+      val response: HttpResponse[String] = Http("https://elpais.com").asString
+      response.body
+    }
 
-    val response: HttpResponse[String] = Http("https://google.com").asString
-    val body = response.body
+    val body = Await.result(futureBody, 5 seconds)
     println(body)
   }
 }
