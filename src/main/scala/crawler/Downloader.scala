326529713d7649c3bd50d23dff187743d81e1c59
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-10-25 22:43:31 +0100

    I learned how to create pipelines!

--- src/main/scala/crawler/Downloader.scala
+++ src/main/scala/crawler/Downloader.scala
@@ -1,9 +1,32 @@
 package crawler
 
-import scala.io.Source
+import org.jsoup.Jsoup
+import org.jsoup.nodes.{Document, Element}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import collection.JavaConverters._
+import scala.collection.mutable
 
 object Downloader {
-//  def download(url: String) = {
-//
-//  }
+
+  def pipeline(url: String): Future[Set[String]] = {
+    getHtml(url).map{doc => parseLinks(doc)}
+  }
+
+  def getHtml(url: String): Future[Document] = {
+    Future {Jsoup.connect(url).get}
+  }
+
+  def parseLinks(doc: Document): Set[String] = {
+    val body: Element = doc.body()
+    val links: mutable.Buffer[Element] = body.select("a").asScala
+
+    val whatever: mutable.Buffer[String] = for {
+      link <- links
+      if link.attr("href").startsWith("/")
+    } yield "https://monzo.com" + link.attr("href")
+
+    whatever.toSet
+  }
 }
--- src/test/scala/crawler/DownloaderSpec.scala
+++ src/test/scala/crawler/DownloaderSpec.scala
@@ -1,24 +1,38 @@
 package crawler
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.{Document, Element}
 import org.scalatest.FunSuite
 import scalaj.http._
+
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import collection.JavaConverters._
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
 class DownloaderSpec extends FunSuite {
   test("I can get the the body of a url"){
-    val doc = Jsoup.connect("http://elpais.com/").get
-    println(doc.body().html().slice(0, 200))
+    val doc = Jsoup.connect("http://monzo.com/").get
+    println(doc.body().html().slice(0, 20))
   }
 
   test("I can use futures to do it async") {
-    val futureDoc = Future {
-      Jsoup.connect("http://elpais.com/").get
-    }
+    val futureDoc = Downloader.getHtml("http://monzo.com")
     val doc = Await.result(futureDoc, 5 seconds)
-    println(doc.body().html().slice(0, 200))
+    println(doc.body().html().slice(0, 20))
+  }
+
+  test("I can parse links ") {
+    val futureDoc: Future[Set[String]] = Downloader.getHtml("http://monzo.com")
+      .map{ doc => Downloader.parseLinks(doc)}
+  }
+
+  test("whole pipeline") {
+    val futureDoc = Downloader.pipeline("http://monzo.com")
+    val doc = Await.result(futureDoc, 5 seconds)
+    println(doc)
   }
 }
